8ad24177c8a8405a0ee1938ec5222adcd49205f6
Daniel <owenjodaniel19@gmail.com>
2023-10-03 01:52:26 +0300

    done with the header

--- yukio/src/components/AccountBtns.js
+++ yukio/src/components/AccountBtns.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const AccountBtns = () => {
-    return 
+  return (
+    <div className="flex items-center font-medium">
+      <a className="hover:text-black transition" href="#">
+        Login
+      </a>
+
+      {/* separator */}
+      <span className="mx-6 text-black font-thin">|</span>
+
+      <button className="btn h-[52px] text-base px-8 text-white-secondary ">Register</button>
+    </div>
+  );
 };
 export default AccountBtns;
--- yukio/src/components/Nav.js
+++ yukio/src/components/Nav.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { navData } from '../data';
 
 const Nav = () => {
-    return 
+    return (
+        <nav className='flex items-center mb-1'>
+            <ul className='flex gap-x-8'>
+                {navData.map((item, index) => {
+                    return (
+                        <li className='border-b-2 border-transparent hover:border-black transition-all duration-300' key={index}>
+                            <a href={item.href}>{item.name}</a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
 };
 export default Nav;
