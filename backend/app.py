345cef1ba3b6b6dc96dcacf59442e2287c5e2a4b
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-10-03 01:05:22 +0300

    created app.py

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///management.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app)
+db.init_app(app)
+
+api = Api(app)
+
+class Index(Resource):
+  def get(self):
+    return 'Welcome to Yukio\'s PMA....' 
+  
+
+if __name__ == '__main__':
+  app.run(port=5555, debug=True)
