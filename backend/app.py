a5bb5ad932c347dc36dff5703a3ee91a47b15f2e
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-10-03 10:50:31 +0300

    defined relationships

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///management.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app)
+migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
