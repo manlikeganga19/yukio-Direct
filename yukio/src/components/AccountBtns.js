7e5585caeab8cc41d096d36ef81ec86772bf9877
Daniel <owenjodaniel19@gmail.com>
2023-10-03 09:47:46 +0300

    login done

--- yukio/src/components/AccountBtns.js
+++ yukio/src/components/AccountBtns.js
@@ -10,8 +10,9 @@ const AccountBtns = () => {
       {/* separator */}
       <span className="mx-6 text-black font-thin">|</span>
 
-      <button className="btn h-[52px] text-base px-8 text-white-secondary ">Register</button>
-    </div>
+      <a href="/register"> <button className="btn h-[52px] text-base px-8 text-white-secondary ">Register</button> </a>
+    </div> 
+      
   );
 };
 export default AccountBtns;
