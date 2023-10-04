91248a0b2a18a73adc796c7377c5e12a74bf935f
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-10-05 02:10:08 +0300

    added delete

--- backend/app.py
+++ backend/app.py
@@ -1,128 +1,128 @@
-# from flask import Flask, make_response, jsonify, request
-# from flask_cors import CORS
-# from flask_migrate import Migrate
-# from flask_restful import Api, Resource
-# from models import db, User, Project, Task
+from flask import Flask, make_response, jsonify, request
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import db, User, Project, Task
 
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///management.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# CORS(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///management.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 
-# migrate = Migrate(app, db)
-# db.init_app(app)
-# api = Api(app)
+migrate = Migrate(app, db)
+db.init_app(app)
+api = Api(app)
 
-# class Index(Resource):
-#   def get(self):
-#     return 'Welcome to Yukio\'s Project Management Application'
-# api.add_resource(Index, '/')
+class Index(Resource):
+  def get(self):
+    return 'Welcome to Yukio\'s Project Management Application'
+api.add_resource(Index, '/')
 
-# #Authentication
-# class Login(Resource):
-#   pass
+#Authentication
+class Login(Resource):
+  pass
 
-# class StayLogged(Resource):
-#   pass
+class StayLogged(Resource):
+  pass
 
 
-# #Routes for dashboard navbar
-# @app.route('/dashboard')
-# def dashboard():
-#   pass
+#Routes for dashboard navbar
+@app.route('/dashboard')
+def dashboard():
+  pass
 
-# @app.route('/dashboard/projects')
-# def show_projects():
-#   projects = Project.query.all()
-#   response_dict = [project.to_dict() for project in projects]
+@app.route('/dashboard/projects')
+def show_projects():
+  projects = Project.query.all()
+  response_dict = [project.to_dict() for project in projects]
+  response = make_response(
+    jsonify(response_dict),
+    200
+  )
+  return response
+
+@app.route('/dashboard/tasks')
+def show_tasks():
+  tasks = Task.query.all()
+  response_dict = [task.to_dict() for task in tasks]
+  response = make_response(
+    jsonify(response_dict),
+    200
+  )
+  return response
+
+# @app.route('/dashboard/profile')
+# def show_profile():
+#   users = User.query.all()
+#   response_dict = [user.to_dict() for user in users]
 #   response = make_response(
 #     jsonify(response_dict),
 #     200
 #   )
 #   return response
 
-# @app.route('/dashboard/tasks')
-# def show_tasks():
-#   tasks = Task.query.all()
-#   response_dict = [task.to_dict() for task in tasks]
-#   response = make_response(
-#     jsonify(response_dict),
-#     200
-#   )
-#   return response
-
-# # @app.route('/dashboard/profile')
-# # def show_profile():
-# #   users = User.query.all()
-# #   response_dict = [user.to_dict() for user in users]
-# #   response = make_response(
-# #     jsonify(response_dict),
-# #     200
-# #   )
-# #   return response
-
-# @app.route('/dashboard/logout')
-# def logout():
-#   pass
+@app.route('/dashboard/logout')
+def logout():
+  pass
   
-# # Create methods
-# @app.route('/dashboard/create_project', methods=['POST'])
-# def create_project():
-#   if request.method == 'POST':
-#         # Get data from the request
-#         data = request.json  # Assuming data is sent in JSON format
+# Create methods
+@app.route('/dashboard/create_project', methods=['POST'])
+def create_project():
+  if request.method == 'POST':
+        # Get data from the request
+        data = request.json  # Assuming data is sent in JSON format
 
-#         # Create a new project
-#         new_project = Project(
-#             title=data['title'],
-#             description=data['description'],
-#             start_date=data['start_date'],
-#             end_date=data['end_date'],
-#             owner_id=data['owner_id']  # You might need to set this based on user authentication
-#         )
+        # Create a new project
+        new_project = Project(
+            title=data['title'],
+            description=data['description'],
+            start_date=data['start_date'],
+            end_date=data['end_date'],
+            owner_id=data['owner_id']  # You might need to set this based on user authentication
+        )
 
-#         # Add the new project to the database
-#         db.session.add(new_project)
-#         db.session.commit()
+        # Add the new project to the database
+        db.session.add(new_project)
+        db.session.commit()
 
-#         return jsonify({"message": "Project created successfully"}, 201)
+        return jsonify({"message": "Project created successfully"}, 201)
   
-#   return jsonify({"error": "Invalid request method"}, 405)
+  return jsonify({"error": "Invalid request method"}, 405)
 
-# @app.route('/dashboard/create_task', methods=['POST'])
-# def create_task():
-#    if request.method == 'POST':
-#         # Get data from the request
-#         data = request.json  
+@app.route('/dashboard/create_task', methods=['POST'])
+def create_task():
+   if request.method == 'POST':
+        # Get data from the request
+        data = request.json  
 
-#         new_task = Task(
-#             name=data['name'],
-#             description=data['description'],
-#             priority=data['priority'],
-#             due_date=data['due_date'],
-#             project_id=data['project_id']  # You might need to set this based on your project structure
-#         )
+        new_task = Task(
+            name=data['name'],
+            description=data['description'],
+            priority=data['priority'],
+            due_date=data['due_date'],
+            project_id=data['project_id']  # You might need to set this based on your project structure
+        )
 
-#         # Add the new task to the database
-#         db.session.add(new_task)
-#         db.session.commit()
+        # Add the new task to the database
+        db.session.add(new_task)
+        db.session.commit()
 
-#         return jsonify({"message": "Task created successfully"}, 201)
-#    return jsonify({"error": "Invalid request method"}, 405)
+        return jsonify({"message": "Task created successfully"}, 201)
+   return jsonify({"error": "Invalid request method"}, 405)
 
-# @app.route('/update_project/<int:project_id>', methods=['PUT'])
-# def update_project_status(project_id):
-#     data = request.json
-#     done = data.get('done')
-#     project = Project.query.get(project_id)
-#     if not project:
-#         return jsonify({"error": "Project not found"}), 404
+@app.route('/update_project/<int:project_id>', methods=['PUT'])
+def update_project_status(project_id):
+    data = request.json
+    done = data.get('done')
+    project = Project.query.get(project_id)
+    if not project:
+        return jsonify({"error": "Project not found"}), 404
 
-#     project.done = done
-#     db.session.commit()
+    project.done = done
+    db.session.commit()
 
-#     return jsonify({"message": "Project updated successfully", "project": project.to_dict()}), 200
+    return jsonify({"message": "Project updated successfully", "project": project.to_dict()}), 200
 
 
-# if __name__ == '__main__':
-#   app.run(port=5555, debug=True)
+if __name__ == '__main__':
+  app.run(port=5555, debug=True)
