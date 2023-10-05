30b8756ff3f75723d1fc82129c2e70f8bd6990b5
Daniel <owenjodaniel19@gmail.com>
2023-10-05 10:30:16 +0300

    updated the dashboard

--- yukio/src/components/Dashboard.js
+++ yukio/src/components/Dashboard.js
@@ -6,7 +6,7 @@ import Footer from './Footer';
 const Dashboard = () => {
     return (
      
-        <div class="wrapper" >
+        <div class="wrapper">
         {/* <!--Top menu --> */}
         <div class="sidebar" >
             {/* <!--profile image & text--> */}
@@ -25,45 +25,28 @@ const Dashboard = () => {
                     <li>
                         <a href="#">
                             <span class="icon"><i class="fas fa-desktop"></i></span>
-                            <span class="item">My Dashboard</span>
+                            <span class="item">profile</span>
                         </a>
                     </li>
                     <li>
                         <a href="#">
                             <span class="icon"><i class="fas fa-user-friends"></i></span>
-                            <span class="item">People</span>
+                            <span class="item">Tasks</span>
                         </a>
                     </li>
                     <li>
                         <a href="#">
                             <span class="icon"><i class="fas fa-tachometer-alt"></i></span>
-                            <span class="item">Perfomance</span>
+                            <span class="item">Project</span>
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            <span class="icon"><i class="fas fa-database"></i></span>
-                            <span class="item">Development</span>
+                            <span class="icon-a"><i class="fas fa-tachometer-alt"></i></span>
+                            <span class="item">Logout</span>
                         </a>
                     </li>
-                    <li>
-                        <a href="#">
-                            <span class="icon"><i class="fas fa-chart-line"></i></span>
-                            <span class="item">Reports</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span class="icon"><i class="fas fa-user-shield"></i></span>
-                            <span class="item">Admin</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span class="icon"><i class="fas fa-cog"></i></span>
-                            <span class="item">Settings</span>
-                        </a>
-                    </li>
+                   
                 </ul>
             </div>
             {/* <!--menu item--> */}
