5453d4eff0781579e461265931dd119f76cae127
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-10-06 12:38:08 +0300

    routing

--- yukio/src/index.js
+++ yukio/src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import Header from "./components/Header"; 
 import { BrowserRouter as Router } from "react-router-dom";
-// import Dashboard from '../src/components/Dashboard/Dashboard'
-// import Profile from './components/Dashboard/Profile';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      {/* <Header/> */}
       <App />
     </Router>
   </React.StrictMode>
