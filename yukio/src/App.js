26299604c8ac757c32a52b4ef024cbbd89d1672c
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-10-09 16:33:54 +0300

    inventory app

---
+++ yukio/src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import RegisterPage from "./pages/Register";
+import LoginPage from "./pages/Login";
+import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
+
+
+const App = () => {
+  const [navMobile, setNavMobile] = useState(false);
+
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  });
+  return (
+    <div classsName="overflow-hidden">
+      <Header setNavMobile={setNavMobile} />
+      <div
+        className={`${
+          navMobile ? "right-0" : "-right-full"
+        } fixed z-10 top-0 h-full transition-all duration-200`}
+      >
+        <NavMobile setNavMobile={setNavMobile} />
+      </div>
+      <div className="max-w-md w-full space-y-8">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+};
+
+export default App;
